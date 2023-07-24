594fe97554d286ef6272cf0f96ea9f523236ba2d
ShatteredTesseract <38566668+ShatteredTesseract@users.noreply.github.com>
2023-07-24 12:41:52 -0500

    Create vibriPose.lua

---
+++ vibriPose.lua
@@ -0,0 +1,21 @@
+{
+    "faction": "/r/vib_ribbon",
+    "contact": "Playstation Rhythm Central",
+    "templates": [
+        {
+            "name": "VibriPose text; Border;",
+            "sources": [
+                "https://cdn.discordapp.com/attachments/1121899132433473679/1133076583440187392/vibriPose4.png"
+            ],
+            "x": 37,
+            "y": 723
+        }
+        
+    ],
+    "whitelist": [
+        
+    ],
+    "blacklist": [
+        
+    ]
+}
