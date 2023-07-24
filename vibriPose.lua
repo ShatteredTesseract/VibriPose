56787367bc4e93adf2312b6832045b02bba7d20e
ShatteredTesseract <38566668+ShatteredTesseract@users.noreply.github.com>
2023-07-24 13:34:32 -0500

    Update vibriPose.lua

--- vibriPose.lua
+++ vibriPose.lua
@@ -5,7 +5,7 @@
         {
             "name": "VibriPose text; Border;",
             "sources": [
-                "https://cdn.discordapp.com/attachments/1121899132433473679/1133076583440187392/vibriPose4.png"
+                "https://cdn.discordapp.com/attachments/1121899132433473679/1133104455043264522/vibriPose5.png"
             ],
             "x": 37,
             "y": 723
